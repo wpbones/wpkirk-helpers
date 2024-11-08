496b97f91b78cf1324a5d609889c66e3959acd0a
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-11-08 18:37:41 +0100

    ✨ adds wpkirk_md, removes useless and improves the rest

--- src/helpers.php
+++ src/helpers.php
@@ -10,18 +10,20 @@ if (!function_exists('wpkirk_toc')) {
    *
    * @return void
    */
-  function wpkirk_toc()
+  function wpkirk_toc($title = "")
   {
     $buffer = ob_get_contents();
 
-    $pattern = '/<h2 class="wpkirk-section" id="([^"]+)">([^<]+)<\/h2>/i';
+    $pattern = '/<h1 class="wpkirk-section" id="([^"]+)">([^<]+)<\/h1>/i';
     $matches = [];
 
     preg_match_all($pattern, $buffer, $matches, PREG_SET_ORDER);
 
     error_log(print_r($matches, true));
 
-    $toc = '<div class="wp-kirk-toc clearfix"><ul>';
+    $toc = '<div class="wp-kirk-toc clearfix">';
+    $toc .= empty($title) ? '' : '<h2>' . $title . '</h2>';
+    $toc .= '<ul>';
 
     foreach ($matches as $match) {
       $id = $match[1];
@@ -37,28 +39,6 @@ if (!function_exists('wpkirk_toc')) {
   }
 }
 
-if (!function_exists('wpkirk_output')) {
-
-  /**
-   * Outputs the result of a function within a details HTML element.
-   *
-   * This function executes the provided function and displays its output
-   * within a details HTML element. The output is formatted based on the specified language.
-   *
-   * @param callable|string $func The function to execute or the code to evaluate.
-   * @param string $language The language for syntax highlighting (default: 'json').
-   * @return void
-   */
-  function wpkirk_output($func, $language = 'json')
-  {
-    echo '<details>';
-    echo '<summary>' . __('Output', 'wp-kirk') . '</summary>';
-    echo wpkirk_execute_code($language, $func);
-    echo '</details>';
-  }
-}
-
-
 if (!function_exists('wpkirk_section')) {
 
   /**
@@ -73,14 +53,14 @@ if (!function_exists('wpkirk_section')) {
    */
   function wpkirk_section($id, $strFunc)
   {
-    echo "<h2 class=\"wpkirk-section\" id=\"$id\">";
+    echo "<hr/><h1 class=\"wpkirk-section\" id=\"$id\">";
     if (is_string($strFunc) && !empty($strFunc)) {
       echo $strFunc;
     }
     if (is_callable($strFunc)) {
       echo $strFunc();
     }
-    echo '</h2>';
+    echo '</h1><hr/>';
   }
 }
 
@@ -92,42 +72,60 @@ if (!function_exists('wpkirk_code')) {
    * This function displays the provided code within a preformatted code block
    * with syntax highlighting based on the specified language.
    *
+   * @param string $func The code to display. If the string starts with "@", the function will load a file.
+   * @param bool $eval Whether to evaluate the code (default: false).
    * @param string $language The language for syntax highlighting (default: 'php').
-   * @param string $func The code to display.
    * @return void
    */
-  function wpkirk_code($language = 'php', $func = '')
+  function wpkirk_code($func = '', $eval = false, $language = 'php', $openDetails = true)
   {
+    // if $func string starts with "@" we will load a file
+    if (substr($func, 0, 1) === '@') {
+      $basePath = WPKirk()->basePath;
+      $filename = ltrim($func, '@');
+      $file = file_get_contents($basePath . $filename);
+
+      $func = htmlspecialchars($file);
+    }
+
+    $replaceBackticksWithCode = function (string $text, string $class = 'inline'): string {
+      $pattern = '/`(.*?)`/';
+      $replacement = '<code class="' . htmlspecialchars($class) . '">$1</code>';
+      return preg_replace($pattern, $replacement, $text);
+    };
+
+    $result = $replaceBackticksWithCode($func);
+
     echo '<pre><code class="language-' . $language . '">';
-    echo $func;
+    echo $result;
     echo '</code></pre>';
+
+    if ($eval) {
+      echo '<details ' . ($openDetails ? 'open' : '') . '>';
+      echo '<summary>' . __('Output', 'wp-kirk') . '</summary>';
+      echo '<pre><code class="language-txt">';
+      echo eval($func);
+      echo '</code></pre>';
+      echo '</details>';
+    }
   }
 }
 
-if (!function_exists('wpkirk_execute_code')) {
-
+if (!function_exists('wpkirk_md')) {
   /**
-   * Executes the provided code and outputs the result within a preformatted code block.
+   * Outputs Markdown text with inline code formatting.
    *
-   * This function executes the provided code (either a callable function or a string)
-   * and displays the result within a preformatted code block with syntax highlighting
-   * based on the specified language.
+   * This function displays the provided Markdown text with inline code formatting.
+   * The code is wrapped in a <code> element with the specified language class.
    *
-   * @param string $language The language for syntax highlighting (default: 'php').
-   * @param callable|string $func The function to execute or the code to evaluate.
+   * @param string $text The Markdown text to display.
+   * @param string $language The language for syntax highlighting (default: '').
    * @return void
    */
-  function wpkirk_execute_code($language = 'php', $func = '')
+  function wpkirk_md(string $text, string $language = '')
   {
-    echo '<pre><code class="language-' . $language . '">';
-    if (is_callable($func)) {
-      echo $func();
-    }
-
-    if (is_string($func)) {
-      echo eval($func);
-    }
-
-    echo '</code></pre>';
+    $pattern = '/`(.*?)`/';
+    $replacement = '<code class="language-' . $language . ' inline">$1</code>';
+    echo preg_replace($pattern, $replacement, $text);
   }
 }
